46f8470016bfdfcbdb4365a1d7a8217861a378c2
Suhas B M <suhaasbm2004@gmail.com>
2025-01-06 01:10:25 +0530

    Design is over

---
+++ script.js
@@ -0,0 +1,8 @@
+const TodoInput=document.getElementById("to-do-input")
+const button=document.getElementById("add-task-btn")
+const listItem=document.getElementById("to-do-list")
+
+button.addEventListener('click',()=>{
+    const data=TodoInput.value.trim()
+    
+})
